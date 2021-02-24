d5df7c93bd510e9eb8e37d32808aab3057c3c178
Felix Dasta <felix.dasta@upr.edu>
2021-02-24 16:35:17 -0400

    First version of navbar

--- client/trackpack/src/App.js
+++ client/trackpack/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Topnav from './components/Topnav'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <Topnav/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
