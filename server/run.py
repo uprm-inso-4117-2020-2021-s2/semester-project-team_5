3416601d3c931b6fb396108c6a96e07d2244d94f
Felix Dasta <felix.dasta@upr.edu>
2021-02-26 16:31:15 -0400

    Navbar sandwich now appears

--- server/run.py
+++ server/run.py
@@ -1,7 +1,11 @@
+from gevent.pywsgi import WSGIServer
 from api.routes import routes
 from api import app
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port= int(os.environ.get("PORT", 5000))
+    app.debug = True
+    wsgi_app = app.wsgi_app
+    http_server = WSGIServer(('', port), wsgi_app)
+    http_server.serve_forever()
